52603c872b1baff83de09d57629d9d7156ab0af6
Nasir <nasirakhtar@hotmail.com>
2017-08-19 20:03:07 +0100

    Initial commit

---
+++ src/com/rsm/HelloWorld.java
@@ -0,0 +1,8 @@
+package com.rsm;
+
+public class HelloWorld {
+    public static void Hello() {
+        // write your code here
+        System.out.print("Hello World");
+    }
+}
---
+++ src/com/rsm/Main.java
@@ -0,0 +1,9 @@
+package com.rsm;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        HelloWorld.Hello();
+    }
+}
